cb66cf84d0d786c374f9deebcd49569dfe827000
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-10-09 17:11:41 -0400

    Added more functionality

--- lib/github-cards.rb
+++ lib/github-cards.rb
@@ -21,6 +21,7 @@ Liquid::Template.register_tag('github-card', Jekyll::GRHG)
 
 require "graphql/client"
 require "graphql/client/http"
+require "time"
 
 module GITHUB
 
@@ -50,24 +51,23 @@ module GITHUB
   query {
     viewer {
       avatarUrl
+      name
       login
       repositories(first:30 privacy:PUBLIC) {
         edges {
           node {
             name
             description
+            primaryLanguage {
+              color
+              name
+            }
+            updatedAt
             stargazers {
               totalCount
             }
             forks {
               totalCount
-            }
-            languages(first: 1 orderBy:{field:SIZE, direction:DESC}) {
-              edges {
-                node {
-                  name
-                }
-              }
             }
           }
         }
@@ -83,31 +83,44 @@ module GITHUB
   username = result.login
   for repo in result.repositories.edges do
     output +=
-# TFW this has to be the formatting or else it looks shit.
-%Q(  <article class="gh-card">
+    %Q(
+  <article class="gh-card">
     <section class="gh-card-top">
       <a href="https://github.com/#{username}"><img class="gh-card-avatar" src="#{avatar_url}" alt="User icon"></a>
-      <div class="gh-card-info">
+      <section class="gh-card-info">
         <h4><a href="https://github.com/#{username}/#{repo.node.name}">#{repo.node.name}</a></h4>
-        <p>Created by <a href="https://github.com/#{username}">#{username}</a></p>
-      </div>
-      <p class="gh-card-lang">#{
-        if repo.node.languages.edges.length != 0
-          repo.node.languages.edges.first.node.name
-        else
-       ""
-        end}</p>
+        <div class="gh-card-details">
+          <p>Created by <a href="https://github.com/#{username}">#{username}</a></p>
+          <p>Last updated on <a href="https://github.com/#{username}/#{repo.node.name}/commits/master"> #{
+            puts repo.node.name + "\t\t\t\t\t\t\t\t\t" + repo.node.updated_at
+            t = Time.parse(repo.node.updated_at)
+                              t.strftime("%Y-%m-%d")}</a></p>
+        </div>
+      </section>
+      #{if repo.node.primary_language
+      %Q(
+        <section class="gh-card-lang">
+      <a class="text-grey">
+        <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" "width "14">
+          <circle cx="7" cy="7" r="7" fill="#{repo.node.primary_language.color}" />
+        </svg>#{repo.node.primary_language.name}</a>
+        </section>)
+      end
+      }
+
     </section>
 
-    <p class="gh-card-desc">#{repo.node.description || "<i>No description provided</i>"}</p>
+    <p class="gh-card-desc">#{repo.node.description || "<i class=\"text-grey\">No description provided</i>"}</p>
 
     <section class="gh-card-bottom">
       <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14">
         <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z" />
-      </svg>#{repo.node.stargazers.total_count.to_s}
+      </svg>
+      <p>#{repo.node.stargazers.total_count.to_s}</p>
       <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 10 16" width="10">
         <path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"/>
-      </svg>#{repo.node.forks.total_count.to_s}
+      </svg>
+      <p>#{repo.node.forks.total_count.to_s}</p>
     </section>
   </article>\n\n)
   end
