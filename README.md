06286c42019a4f1a0162477547f2d0c530e32376
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-10-09 23:47:10 -0400

    started to implement args turned showing the primary language into a function Made readme useful

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # Github Cards
-## *Because I can't think of a better name*
 
-Github Cards is a tool to create an easy-to-reference HTML snippet that can be used for anything that supports Liquid syntax.
+Github Cards is a Jekyll to create an easy-to-reference and stylable GitHub Card.
+
+## Installation
+
+This is still an in-progress work.
+
+In your jekyll `Gemfile` add the gem `graphql-client` to the Jekyll plugins group:
+
+```
+group :jekyll_plugins do
+
+   ...
+
+   gem "graphql-client"
+end
+```
+
+Then, re-run `bundle install`.
+
+You'll also need to do something to add your token, I'll figure out how to make that easier for using your own.
+
+## Usage
+
+By default, Github Cards only shows HTML. This is intentional, as the user can then style it themselves. However,
+I have provided two files as a template: `style.css` and `_github-cards.scss`.
+
+They both provide the same functionality. Note that you may need to increase specificity if your other CSS/Sass
+overrides these styles.
+
+To list all your repos, add the following liquid tag:
+```
+{% ghcards %}
+```
+
+This will produce cards for the first 30 most recently created repos. This is useful for listing out your repos for a projects page.
+
+To show a single repo, add the following liquid tag:
+```
+{% ghcards *username* *repo* %}
+```
+For example, if you wanted to show only this repo, you'd use:
+```
+{% ghcards edward-shen github-cards %}
+```
+
+## Planned features
+- [ ] Specify single card
+- [ ] Implement get n cards from your repo
+- [ ] Bug fixes
--- lib/github-cards.rb
+++ lib/github-cards.rb
@@ -54,7 +54,7 @@ class GithubCards < Liquid::Tag
       avatarUrl
       name
       login
-      repositories(first:30 privacy:PUBLIC) {
+      repositories(first:30 privacy:PUBLIC orderBy:{field:CREATED_AT direction:DESC}) {
         edges {
           node {
             name
@@ -77,64 +77,70 @@ class GithubCards < Liquid::Tag
   }
   GRAPHQL
 
+  @@args = nil
 
-  def initialize(tag_name, username, tokens)
+  def initialize(tag_name, args, tokens)
+    @@args = args.split(" ")
     super
   end
 
   def render(context)
-
-  result = GithubCards::Client.query(RepoQuery).data.viewer
-  output = "<section class=\"gh-cards\">\n"
-  avatar_url = result.avatar_url
-  username = result.login
-  for repo in result.repositories.edges do
-    output +=
-    %Q(
-  <article class="gh-card">
-    <section class="gh-card-top">
-      <a href="https://github.com/#{username}"><img class="gh-card-avatar" src="#{avatar_url}" alt="User icon"></a>
-      <section class="gh-card-info">
-        <a href="https://github.com/#{username}/#{repo.node.name}"><h4>#{repo.node.name}</h4></a>
-        <div class="gh-card-details">
-          <p>Created by <a href="https://github.com/#{username}">#{username}</a></p>
-          <p>Last updated on <a href="https://github.com/#{username}/#{repo.node.name}/commits/master"> #{
-            t = Time.parse(repo.node.pushed_at)
-            t.strftime("%Y-%m-%d")}</a></p>
-        </div>
-      </section>
-      #{if repo.node.primary_language
-      %Q(
-        <section class="gh-card-lang">
-      <p class="text-grey">
-        #{repo.node.primary_language.name}</p>
-        <svg aria-hidden="true" version="1.1" viewBox="0 0 14 16">
-        <circle cx="7" cy="7" r="7" fill="#{repo.node.primary_language.color}" />
-      </svg>
-        </section>)
-      end
-      }
-
-    </section>
-
-    <p class="gh-card-desc">#{repo.node.description || "<i class=\"text-grey\">No description provided.</i>"}</p>
-
-    <section class="gh-card-bottom text-grey">
-      <svg aria-hidden="true" version="1.1" viewBox="0 0 14 16">
-        <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z" />
-      </svg>
-      <p>#{repo.node.stargazers.total_count.to_s}</p>
-      <svg aria-hidden="true" version="1.1" viewBox="0 0 10 16">
-        <path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"/>
-      </svg>
-      <p>#{repo.node.forks.total_count.to_s}</p>
-    </section>
-  </article>\n\n)
+    if !@@args[0]
+      showAll
+    end
   end
-  output += "</section>"
-  # output += "</section>\n<style>#{File.read('style.css')}</style>"
-  # File.write("testfile.html", output)
+
+  def showAll()
+    result = GithubCards::Client.query(RepoQuery).data.viewer
+    output = "<section class=\"gh-cards\">\n"
+    avatar_url = result.avatar_url
+    username = result.login
+    for repo in result.repositories.edges do
+      output += %Q(
+        <article class="gh-card">
+          <section class="gh-card-top">
+            <a href="https://github.com/#{username}"><img class="gh-card-avatar" src="#{avatar_url}" alt="User icon"></a>
+            <section class="gh-card-info">
+              <a href="https://github.com/#{username}/#{repo.node.name}"><h4>#{repo.node.name}</h4></a>
+              <div class="gh-card-details">
+                <p>Created by <a href="https://github.com/#{username}">#{username}</a></p>
+                <p>Last updated on <a href="https://github.com/#{username}/#{repo.node.name}/commits/master"> #{
+                  t = Time.parse(repo.node.pushed_at)
+                  t.strftime("%Y-%m-%d")}</a>
+                </p>
+              </div>
+            </section>
+            #{showRepoLanguage(repo)}
+          </section>
+
+          <p class="gh-card-desc">#{repo.node.description || "<i class=\"text-grey\">No description provided.</i>"}</p>
+
+          <section class="gh-card-bottom text-grey">
+            <svg aria-hidden="true" version="1.1" viewBox="0 0 14 16">
+              <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z" />
+            </svg>
+            <p>#{repo.node.stargazers.total_count.to_s}</p>
+            <svg aria-hidden="true" version="1.1" viewBox="0 0 10 16">
+              <path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"/>
+            </svg>
+            <p>#{repo.node.forks.total_count.to_s}</p>
+          </section>
+          </article>\n\n)
+    end
+    output += "</section>"
+  end
+
+  def showRepoLanguage(repo)
+    if repo.node.primary_language
+      %Q(
+      <section class="gh-card-lang">
+        <p class="text-grey">#{repo.node.primary_language.name}</p>
+        <svg aria-hidden="true" version="1.1" viewBox="0 0 14 16">
+          <circle cx="7" cy="7" r="7" fill="#{repo.node.primary_language.color}" />
+        </svg>
+      </section>)
+    end
   end
 end
 
-Liquid::Template.register_tag('ghcard', GithubCards)
+Liquid::Template.register_tag('ghcards', GithubCards)
